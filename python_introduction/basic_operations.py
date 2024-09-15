8dfed19d27699554b9ecca71b0f06dd305f14826
Moment49 <ibenachoelvis49@gmail.com>
2024-09-15 21:51:33 +0100

    written the basic operations program

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Store the numbers in vaiables number1 and number2
+number1 = 10
+number2 = 5
+
+# Perform Addition 
+result = number1 + number2
+print(f"Addition of {number1} and {number2} is {result}")
+
+# Perform Subtraction
+result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {result}")
+
+# Perform Multiplication
+result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {result}")
