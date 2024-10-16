d53ec6daf98f13f655bec65915adf819d5bd0124
Moment49 <ibenachoelvis49@gmail.com>
2024-10-16 13:46:29 +0100

    Added the library class and implemented the composition inheritance

--- oop/library_system.py
+++ oop/library_system.py
@@ -14,7 +14,7 @@ class EBook(Book):
         self.file_size = file_Size
         
     def __str__(self):
-        return f"{self.title} {self.author}, File Size:{self.file_size}"
+        return f"{self.title} {self.author}, File Size:{self.file_size} KB"
     
 
 class PrintBook(Book):
