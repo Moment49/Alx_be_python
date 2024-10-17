7f7486cfc6b42fd78d56e7f22e8d3f4c30b94208
Moment49 <ibenachoelvis49@gmail.com>
2024-10-17 10:57:06 +0100

    completed the polymorphism in OOP

---
+++ programming_paradigm/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Area must be overriddent by the child classes")
+
+
+class Rectangle(Shape):
+    def __init__(self, lenght,  width):
+        super().__init__()
+        self.lenght = lenght
+        self.width = width
+    
+    def area(self):
+        return self.lenght * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+    
+    def area(self):
+        return math.pi * self.radius**2
